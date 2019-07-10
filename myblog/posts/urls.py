26e946e1e1370ebb8f4b729918edeab6450cd0c8
CLAB1 <rittyt@yahoo.com>
2019-07-10 15:32:48 +0530

    My first django project

--- myblog/posts/urls.py
+++ myblog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from posts.views import post_list,post_detail
+from posts.views import post_list,post_detail,new_post
 
 urlpatterns=[
     url('^$',post_list,name='post_list'),
     path('posts/<int:pk>/',post_detail,name='post_detail'),
+    path('posts/new/',new_post,name='new_post')
 
 ]
--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from posts.models import Post
+from posts.forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -9,4 +10,16 @@ def post_list(request):
 def post_detail(request,pk):
     post=Post.objects.get(id=pk)
     return render(request,'post/post_detail.html',{"post":post})
+
+def new_post(request):
+    if(request.method=="POST"):
+        form=PostForm(request.POST)
+        if(form.is_valid()):
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm()
+        return render(request,'post/new_post.html',{"form":form})
 
